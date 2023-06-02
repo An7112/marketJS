0428f0430c2b86c44e47fb37f9dbf1915f538409
An7112 <an.nguyent@gotecq.com>
2023-06-02 19:43:42 +0700

    update: sidebar link

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 function toggleSidebar() {
-    var sidebar = document.querySelector('.sidebar');
+    var sidebar = document.getElementById('sidebar');
     var content = document.querySelector('.content');
-
     sidebar.classList.toggle('open');
-    content.classList.toggle('open')
-}
+    content.classList.toggle('open');
+  }
+  
+  function removeClassSidebar() {
+    var sidebar = document.getElementById('sidebar');
+    var content = document.querySelector('.content');
+    sidebar.classList.remove('open');
+    content.classList.remove('open');
+  }
+  
+  function clickOutside(event) {
+    var sidebar = document.getElementById('sidebar');
+    var header = document.getElementById('header');
+    if (!sidebar.contains(event.target) && !header.contains(event.target)) {
+      removeClassSidebar();
+    }
+  }
+  
+  document.addEventListener('click', clickOutside);
+  
